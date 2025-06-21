efe0833bf48627a2c0a8913c0fb4258ae065bedf
Clint Harrison <243356+clintharrison@users.noreply.github.com>
2025-06-21 18:57:27 -0400

    Lie about the glib version... this matches what's on a kindle

--- patch/any/usr/lib/glib-2.0/include/glibconfig.h
+++ patch/any/usr/lib/glib-2.0/include/glibconfig.h
@@ -114,8 +114,8 @@ typedef unsigned long guintptr;
 #define G_GUINTPTR_FORMAT       "lu"
 
 #define GLIB_MAJOR_VERSION 2
-#define GLIB_MINOR_VERSION 82
-#define GLIB_MICRO_VERSION 4
+#define GLIB_MINOR_VERSION 29
+#define GLIB_MICRO_VERSION 18
 
 #define G_OS_UNIX
 
