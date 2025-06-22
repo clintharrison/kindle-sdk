733a4c5252f8a504724f4b711b3c0ecd4c4fcd47
Clint Harrison <243356+clintharrison@users.noreply.github.com>
2025-06-21 21:14:00 -0400

    Downgrade to atk 1.30.0

--- patch/any/usr/include/atk-1.0/atk/atkaction.h
+++ patch/any/usr/include/atk-1.0/atk/atkaction.h
@@ -17,24 +17,30 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#ifndef __ATK_ACTION_H__
-#define __ATK_ACTION_H__
-
-#if defined(ATK_DISABLE_SINGLE_INCLUDES) && !defined(__ATK_H_INSIDE__) && !defined(ATK_COMPILATION)
+#if defined(ATK_DISABLE_SINGLE_INCLUDES) && !defined (__ATK_H_INSIDE__) && !defined (ATK_COMPILATION)
 #error "Only <atk/atk.h> can be included directly."
 #endif
+
+#ifndef __ATK_ACTION_H__
+#define __ATK_ACTION_H__
 
 #include <atk/atkobject.h>
 
 G_BEGIN_DECLS
 
 /*
+ * The interface AtkAction should be supported by any object that can 
+ * perform one or more actions. The interface provides the standard 
+ * mechanism for an assistive technology to determine what those actions 
+ * are as well as tell the object to perform them. Any object that can 
+ * be manipulated should support this interface.
  */
 
-#define ATK_TYPE_ACTION (atk_action_get_type ())
-#define ATK_IS_ACTION(obj) G_TYPE_CHECK_INSTANCE_TYPE ((obj), ATK_TYPE_ACTION)
-#define ATK_ACTION(obj) G_TYPE_CHECK_INSTANCE_CAST ((obj), ATK_TYPE_ACTION, AtkAction)
-#define ATK_ACTION_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), ATK_TYPE_ACTION, AtkActionIface))
+
+#define ATK_TYPE_ACTION                    (atk_action_get_type ())
+#define ATK_IS_ACTION(obj)                 G_TYPE_CHECK_INSTANCE_TYPE ((obj), ATK_TYPE_ACTION)
+#define ATK_ACTION(obj)                    G_TYPE_CHECK_INSTANCE_CAST ((obj), ATK_TYPE_ACTION, AtkAction)
+#define ATK_ACTION_GET_IFACE(obj)          (G_TYPE_INSTANCE_GET_INTERFACE ((obj), ATK_TYPE_ACTION, AtkActionIface))
 
 #ifndef _TYPEDEF_ATK_ACTION_
 #define _TYPEDEF_ATK_ACTION_
@@ -42,45 +48,27 @@ typedef struct _AtkAction AtkAction;
 #endif
 typedef struct _AtkActionIface AtkActionIface;
 
-/**
- * AtkActionIface:
- * @do_action:
- * @get_n_actions:
- * @get_description:
- * @get_name:
- * @get_keybinding:
- * @set_description:
- * @get_localized_name:
- *
- * The #AtkAction interface should be supported by any object that can
- * perform one or more actions. The interface provides the standard
- * mechanism for an assistive technology to determine what those actions
- * are as well as tell the object to perform them. Any object that can
- * be manipulated should support this interface.
- */
 struct _AtkActionIface
 {
-  /*< private >*/
   GTypeInterface parent;
 
-  /*< public >*/
-  gboolean (*do_action) (AtkAction *action,
-                         gint i);
-  gint (*get_n_actions) (AtkAction *action);
-  const gchar *(*get_description) (AtkAction *action,
-                                   gint i);
-  const gchar *(*get_name) (AtkAction *action,
-                            gint i);
-  const gchar *(*get_keybinding) (AtkAction *action,
-                                  gint i);
-  gboolean (*set_description) (AtkAction *action,
-                               gint i,
-                               const gchar *desc);
-  const gchar *(*get_localized_name) (AtkAction *action,
-                                      gint i);
+  gboolean                (*do_action)         (AtkAction         *action,
+                                                gint              i);
+  gint                    (*get_n_actions)     (AtkAction         *action);
+  G_CONST_RETURN gchar*   (*get_description)   (AtkAction         *action,
+                                                gint              i);
+  G_CONST_RETURN gchar*   (*get_name)          (AtkAction         *action,
+                                                gint              i);
+  G_CONST_RETURN gchar*   (*get_keybinding)    (AtkAction         *action,
+                                                gint              i);
+  gboolean                (*set_description)   (AtkAction         *action,
+                                                gint              i,
+                                                const gchar       *desc);
+  G_CONST_RETURN gchar*   (*get_localized_name)(AtkAction         *action,
+						gint              i);
+  AtkFunction             pad2;
 };
 
-ATK_AVAILABLE_IN_ALL
 GType atk_action_get_type (void);
 
 /*
@@ -94,29 +82,23 @@ GType atk_action_get_type (void);
  * provided in a support library
  */
 
-ATK_AVAILABLE_IN_ALL
-gboolean atk_action_do_action (AtkAction *action,
-                               gint i);
-ATK_AVAILABLE_IN_ALL
-gint atk_action_get_n_actions (AtkAction *action);
-ATK_AVAILABLE_IN_ALL
-const gchar *atk_action_get_description (AtkAction *action,
-                                         gint i);
-ATK_AVAILABLE_IN_ALL
-const gchar *atk_action_get_name (AtkAction *action,
-                                  gint i);
-ATK_AVAILABLE_IN_ALL
-const gchar *atk_action_get_keybinding (AtkAction *action,
-                                        gint i);
-ATK_AVAILABLE_IN_ALL
-gboolean atk_action_set_description (AtkAction *action,
-                                     gint i,
-                                     const gchar *desc);
+gboolean   atk_action_do_action                (AtkAction         *action,
+                                            gint              i);
+gint   atk_action_get_n_actions            (AtkAction *action);
+G_CONST_RETURN gchar* atk_action_get_description  (AtkAction         *action,
+                                                   gint              i);
+G_CONST_RETURN gchar* atk_action_get_name         (AtkAction         *action,
+                                                   gint              i);
+G_CONST_RETURN gchar* atk_action_get_keybinding   (AtkAction         *action,
+                                                   gint              i);
+gboolean              atk_action_set_description  (AtkAction         *action,
+                                                   gint              i,
+                                                   const gchar       *desc);
 
 /* NEW in ATK 1.1: */
-ATK_AVAILABLE_IN_ALL
-const gchar *atk_action_get_localized_name (AtkAction *action,
-                                            gint i);
+
+G_CONST_RETURN gchar* atk_action_get_localized_name (AtkAction       *action,
+						     gint            i);
 
 /*
  * Additional GObject properties exported by AtkAction:
--- patch/any/usr/include/atk-1.0/atk/atkgobjectaccessible.h
+++ patch/any/usr/include/atk-1.0/atk/atkgobjectaccessible.h
@@ -17,14 +17,14 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#ifndef __ATK_GOBJECT_ACCESSIBLE_H__
-#define __ATK_GOBJECT_ACCESSIBLE_H__
-
-#if defined(ATK_DISABLE_SINGLE_INCLUDES) && !defined(__ATK_H_INSIDE__) && !defined(ATK_COMPILATION)
+#if defined(ATK_DISABLE_SINGLE_INCLUDES) && !defined (__ATK_H_INSIDE__) && !defined (ATK_COMPILATION)
 #error "Only <atk/atk.h> can be included directly."
 #endif
 
-#include <atk/atkobject.h>
+#ifndef __ATK_GOBJECT_ACCESSIBLE_H__
+#define __ATK_GOBJECT_ACCESSIBLE_H__
+
+#include <atk/atk.h>
 
 G_BEGIN_DECLS
 
@@ -32,22 +32,21 @@ G_BEGIN_DECLS
  * The AtkGObjectAccessible class is provided as a basis for implementing
  * accessibility support for objects which are not GTK+ widgets
  */
-#define ATK_TYPE_GOBJECT_ACCESSIBLE (atk_gobject_accessible_get_type ())
-#define ATK_GOBJECT_ACCESSIBLE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessible))
-#define ATK_GOBJECT_ACCESSIBLE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessibleClass))
-#define ATK_IS_GOBJECT_ACCESSIBLE(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE))
+#define ATK_TYPE_GOBJECT_ACCESSIBLE            (atk_gobject_accessible_get_type ())
+#define ATK_GOBJECT_ACCESSIBLE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessible))
+#define ATK_GOBJECT_ACCESSIBLE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessibleClass))
+#define ATK_IS_GOBJECT_ACCESSIBLE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE))
 #define ATK_IS_GOBJECT_ACCESSIBLE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), ATK_TYPE_GOBJECT_ACCESSIBLE))
-#define ATK_GOBJECT_ACCESSIBLE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessibleClass))
+#define ATK_GOBJECT_ACCESSIBLE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), ATK_TYPE_GOBJECT_ACCESSIBLE, AtkGObjectAccessibleClass))
 
-typedef struct _AtkGObjectAccessible AtkGObjectAccessible;
-typedef struct _AtkGObjectAccessibleClass AtkGObjectAccessibleClass;
+typedef struct _AtkGObjectAccessible                AtkGObjectAccessible;
+typedef struct _AtkGObjectAccessibleClass           AtkGObjectAccessibleClass;
 
 struct _AtkGObjectAccessible
 {
   AtkObject parent;
 };
 
-ATK_AVAILABLE_IN_ALL
 GType atk_gobject_accessible_get_type (void);
 
 struct _AtkGObjectAccessibleClass
@@ -58,10 +57,8 @@ struct _AtkGObjectAccessibleClass
   AtkFunction pad2;
 };
 
-ATK_AVAILABLE_IN_ALL
-AtkObject *atk_gobject_accessible_for_object (GObject *obj);
-ATK_AVAILABLE_IN_ALL
-GObject *atk_gobject_accessible_get_object (AtkGObjectAccessible *obj);
+AtkObject *atk_gobject_accessible_for_object      (GObject           *obj);
+GObject   *atk_gobject_accessible_get_object      (AtkGObjectAccessible *obj);
 
 G_END_DECLS
 
